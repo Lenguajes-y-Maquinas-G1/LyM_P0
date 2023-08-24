b88234b6b44aa8b75119fdc3fb9d50b903433109
Juan E Pinto <je.pinto@uniandes.edu.co>
2023-08-23 20:03:25 -0500

    Funcion que retorna la posicion actual del robot

--- codigo.py
+++ codigo.py
@@ -7,5 +7,27 @@ def crear_matriz(n:int):
         matriz.append(filas)
     return matriz
 matriz=crear_matriz(3)
+matriz[2][1]=1
 for i in matriz:
     print (i)
+
+def posicion():
+    fila=0
+
+    for fila in range (len(matriz)):
+        if 1 in matriz[fila]:
+            for valor in matriz[fila]:
+                if valor ==1:
+                    return (fila,valor)
+                    break
+
+
+    pass
+
+print(posicion())
+
+
+
+
+def walk(v:int,d:str):
+    pass
