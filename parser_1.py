cea09f0f8ae505054081555a57ee32adac792f1d
scasasbuenas <s.casasbuenas@uniandes.edu.co>
2023-08-28 20:32:34 -0500

    Inicial

--- parser_1.py
+++ parser_1.py
@@ -9,6 +9,26 @@ blank_strings = token_list.count("")
 for tabulate in range(0, tabulates): token_list.remove("\t")
 for blank_string in range(0, blank_strings): token_list.remove("")
 
+print(token_list)
+
 reserved_structures = ["defvar", "defproc", "while", "else", "if", "repeat", "times"]
 
-def check_code():
+def def_var(counter):
+    if token_list[counter + 1] != "":
+        pass
+    else:
+        return False
+    
+    if token_list[counter + 2].isnumeric:
+        pass
+    else:
+        return False
+
+i = 0
+
+while i > len(token_list):
+    answer = def_var(i)
+    
+       
+
+    
